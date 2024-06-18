376b5d8f1639f5495af7ea07a181ce2bce92aa67
[AR Lunan] <arlunan@ieee.org>
2024-06-18 12:29:12 -0400

    Rivised no gui-description launch

--- mtbc1_description/launch/mtbc1_description.launch.py
+++ mtbc1_description/launch/mtbc1_description.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
     #default_model_path = os.path.join(pkg_share, 'src/description/mtbc1_description.urdf')
     default_model_path = os.path.join(pkg_share, 'urdf/mtbc1.urdf.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf.config.rviz')
-
+    
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -21,12 +21,14 @@ def generate_launch_description():
         arguments=[default_model_path],
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
+    
     joint_state_publisher_gui_node = launch_ros.actions.Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
+    
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
